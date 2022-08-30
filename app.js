8ed15ab8b4ef3416359635647c56ab555b930f76
M-Fahad786 <fahad.zahid2004@gmail.com>
2022-08-30 18:28:30 +0500

    Objects

---
+++ app.js
@@ -0,0 +1,69 @@
+/*
+let obj = {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+console.log(obj);
+
+let obj2 = {
+  names: ["Fahad", "Zahid", "Mustafa"],
+  age: [18, 45, 10],
+};
+console.log(obj2);
+
+let obj3 = {};
+obj3.name = "Fahad";
+obj3.age = 18;
+obj3.studies = "Bachelor";
+obj3.city = "Karachi";
+console.log(obj3);
+
+let obj4 = {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+for (let key in obj4) {
+  console.log(
+    "The following value of" + " " + key + " " + "is" + " " + obj4[key]
+  );
+}
+
+let obj5= {
+  Name: "Tesla X",
+  Color: "Black",
+  Model: 2021,
+  Manufacturer: "Tesla",
+  Specifications: "Auto Drive Feature",
+  Wheels: "Alloy",
+};
+console.table(obj5)
+
+
+function Objects(name, price, space, transfer, pages) {
+  this.name = name;
+  this.price = price;
+  this.space = space;
+  this.transfer = transfer;
+  this.pages = pages;
+}
+
+var plan1 = new Objects("basic", 20, "100gb", "6mb/sec" , 5)
+var plan2 = new Objects("advance", 50, "100gb", "6mb/sec", 10)
+var plan3 = new Objects("ultimate", 80, "100gb", "6mb/sec", 30)
+var plan4 = new Objects("professional", 100, "100gb", "6mb/sec", 50)
+console.log(plan1)
+console.log(plan2)
+console.log(plan3)
+console.log(plan4)
+*/
+
+
+
